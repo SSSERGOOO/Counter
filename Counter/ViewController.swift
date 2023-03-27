5666532cbbe73b48c09350cd2c5e6a565325d5bf
Sergey <sergowinwinner@gmail.com>
2023-03-27 23:04:49 +0300

    Created counter app.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,9 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var counter = 0
+    
+    @IBOutlet weak var Button: UIButton!
+        
+    @IBOutlet weak var counterValue: UILabel!
+    
+    @IBAction func buttonDidTap(_ sender: UIButton) {
+        counter += 1
+        counterValue.text = String(counter)
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        counterValue.text = "0"
+        
+    
         // Do any additional setup after loading the view.
     }
 
